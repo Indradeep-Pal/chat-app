846c63d747c6125350021d52d2b7d1a304940202
Indradeep-Pal <124890968+Indradeep-Pal@users.noreply.github.com>
2023-07-04 00:21:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ dotenv.config()
 const app = express()
 
 app.use(express.json())
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 
 const connect=async()=>{
